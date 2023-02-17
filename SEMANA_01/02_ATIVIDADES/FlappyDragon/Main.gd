31670fc501777dfd4d91b6cbc46e46ed7f6b0661
CaioKakunaka <124264819+CaioKakunaka@users.noreply.github.com>
2023-02-17 16:00:33 -0300

    .

--- SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
+++ SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
@@ -1,9 +1,9 @@
 extends Node # instancia a classe Node2D
 
 var status = 1
-var vscore = 0
-var x = 1.5 
-var y = 1.5 
+var vscore = 100
+var x = 1.5
+var y = 1.5	
 
 # executa essa função ao carregar o jogo
 func _ready():
